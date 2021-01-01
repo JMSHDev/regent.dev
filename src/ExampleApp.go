8ca9b700b7a626074ead16e13e53f5d9763fb26d
Stuart Holliday <pogsquog@gmail.com>
2021-01-01 16:16:24 +0000

    Added example app to IoTEnable. Added configuration json file and auto create of default.

---
+++ src/ExampleApp.go
@@ -0,0 +1,9 @@
+package main
+
+import "time"
+
+func main() {
+	print("Zzzzzz")
+	time.Sleep(5 * time.Second)
+	print("Done")
+}
--- src/IoTEnabler.go
+++ src/IoTEnabler.go
@@ -1,42 +1,74 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"net"
-	"strconv"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
 )
 
-type udpServer struct {
-	name string
-	port int
-}
-
-type httpClient struct {
-	name string
-	url  string
+type Config struct {
+	PathToExecutable string
+	Arguments        string
 }
 
 func main() {
-	serverConfig := udpServer{"UDP1", 8081}
+	config := loadConfig()
+	log.Printf("%v\n", config)
 
-	CONNECT := "0.0.0.0:" + strconv.Itoa(serverConfig.port)
-
-	s, err := net.ResolveUDPAddr("udp4", CONNECT)
-	c, err := net.ListenUDP("udp4", s)
+	cmd := exec.Command(config.PathToExecutable, config.Arguments)
+	_, err := cmd.StdinPipe()
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 
-	defer c.Close()
+	var out bytes.Buffer
+	cmd.Stdout = &out
 
-	for {
-		buffer := make([]byte, 1024)
-		n, _, err := c.ReadFromUDP(buffer)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Printf("Reply: %s\n", string(buffer[0:n]))
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Printf("%q\n", out.String())
+}
+
+func loadConfig() Config {
+	f, err := os.Open("config.json")
+	if err != nil {
+		//no valid config found - make a new one and return it
+		saveDefaultConfig()
+		log.Fatal("No config found - saving default and quiting")
+	}
+	defer f.Close()
+
+	data, _ := ioutil.ReadAll(f)
+
+	var config Config
+	jsonErr := json.Unmarshal(data, &config)
+	if jsonErr != nil {
+		log.Printf("%v\n", jsonErr)
+	}
+	return config
+}
+
+func saveDefaultConfig() Config {
+	f, err := os.Create("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	defaultConfig := Config{
+		PathToExecutable: "fish",
+		Arguments:        "face",
+	}
+	jsonValue, err := json.MarshalIndent(defaultConfig, "", "  ")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	f.Write(jsonValue)
+	return defaultConfig
 }
