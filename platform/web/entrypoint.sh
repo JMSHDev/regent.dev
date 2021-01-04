3d56a8f79553b56b5545655ac2e060fcaebd1041
jacek <jacek.miloszewski@gmail.com>
2021-01-04 22:57:05 +0000

    added vue boilerplate

--- platform/web/entrypoint.sh
+++ platform/web/entrypoint.sh
@@ -3,6 +3,7 @@
 python manage.py migrate
 python manage.py createsuperuser --noinput
 python manage.py collectstatic --noinput --clear
+cp -r /app/dist/* /app/static
 
 daphne -b 0.0.0.0 -p 8000 api.asgi:application
 
