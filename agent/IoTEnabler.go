d55523a1a51e0da020508f1305fb2d7b44eb1027
Stuart Holliday <pogsquog@gmail.com>
2021-01-12 01:06:21 +0000

    Refactored config into its own file

--- agent/IoTEnabler.go
+++ agent/IoTEnabler.go
@@ -3,26 +3,16 @@ package main
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
 	"os/exec"
 	"time"
 )
 
-type Config struct {
-	PathToExecutable string
-	Arguments        string
-	AutoRestart      bool
-	RestartDelayMs   int
-}
-
 func main() {
-	config, err := loadConfig()
+	config, err := LoadConfig()
 	if err != nil {
-		saveDefaultConfig()
+		SaveDefaultConfig()
 		log.Fatal("Config not found - created default")
 	}
 
@@ -60,43 +50,4 @@ func LaunchProcess(pathToExecutable string, arguments string) {
 		log.Fatal(err)
 	}
 	fmt.Printf("%q\n", out.String())
-}
-
-func loadConfig() (Config, error) {
-	f, err := os.Open("config.json")
-	if err != nil {
-		//no valid config found - make a new one and return it
-		return Config{}, err
-	}
-	defer f.Close()
-
-	data, _ := ioutil.ReadAll(f)
-
-	var config Config
-	jsonErr := json.Unmarshal(data, &config)
-	if jsonErr != nil {
-		log.Printf("%v\n", jsonErr)
-	}
-	return config, nil
-}
-
-func saveDefaultConfig() Config {
-	f, err := os.Create("config.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	defaultConfig := Config{
-		PathToExecutable: "fish",
-		Arguments:        "face",
-		AutoRestart:      true,
-		RestartDelayMs:   10000,
-	}
-	jsonValue, err := json.MarshalIndent(defaultConfig, "", "  ")
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-	f.Write(jsonValue)
-	return defaultConfig
 }
---
+++ agent/config.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+type Config struct {
+	PathToExecutable string
+	Arguments        string
+	AutoRestart      bool
+	RestartDelayMs   int
+}
+
+func LoadConfig() (Config, error) {
+	f, err := os.Open("config.json")
+	if err != nil {
+		//no valid config found - make a new one and return it
+		return Config{}, err
+	}
+	defer f.Close()
+
+	data, _ := ioutil.ReadAll(f)
+
+	var config Config
+	jsonErr := json.Unmarshal(data, &config)
+	if jsonErr != nil {
+		log.Printf("%v\n", jsonErr)
+	}
+	return config, nil
+}
+
+func SaveDefaultConfig() Config {
+	f, err := os.Create("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	defaultConfig := Config{
+		PathToExecutable: "fish",
+		Arguments:        "face",
+		AutoRestart:      true,
+		RestartDelayMs:   10000,
+	}
+	jsonValue, err := json.MarshalIndent(defaultConfig, "", "  ")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	f.Write(jsonValue)
+	return defaultConfig
+}
