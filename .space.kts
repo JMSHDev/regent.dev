4169870928d37fba874479b0cef406fa32f683ec
Stuart Holliday <stuart.holliday@irisys.co.uk>
2021-02-04 01:45:43 +0000

    Added space build job

---
+++ .space.kts
@@ -0,0 +1,16 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+
+job("Build agent and ExampleApp") {
+    container("golang:buster") {
+        shellScript {
+            interpreter = "/bin/bash"
+            location = "build.sh"
+            //args("--my-first-arg", "--my-second-arg")
+        }
+    }
+}
