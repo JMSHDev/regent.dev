b161508eb2d1c81f4c0dea7192d682a41fd4e96c
Stuart Holliday <stuart.holliday@irisys.co.uk>
2021-02-07 01:32:01 +0000

    Refactored process into its own file

--- agent/IoTEnabler.go
+++ agent/IoTEnabler.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"context"
-	"fmt"
-	"io"
 	"log"
 	"os"
-	"os/exec"
 	"os/signal"
 	"sync"
 	"syscall"
-	"time"
 )
 
 func main() {
@@ -24,7 +18,7 @@ func main() {
 	log.Printf("%v\n", config)
 
 	mqttServer := MQTTServerDetails{
-		address:  "localhost:1883",
+		address:  "FlamingHellfish:1883",
 		username: "",
 		password: "",
 	}
@@ -34,7 +28,7 @@ func main() {
 	var waitGroup sync.WaitGroup // wait for everything to finish so can safely shutdown
 
 	go subscribeToMqttServer(mqttServer, &waitGroup, config.DeviceID, mqttMessages)
-	launchProcess(config.PathToExecutable,
+	LaunchProcess(config.PathToExecutable,
 		config.Arguments,
 		processMessages,
 		mqttMessages,
@@ -64,87 +58,4 @@ func main() {
 	print("Waiting for completion\n")
 	waitGroup.Wait()
 	print("done\n")
-}
-
-func launchProcess(
-	pathToExecutable string,
-	arguments string,
-	inputMessages chan string,
-	mqttMessages chan MQTTMessage,
-	autoRestart bool,
-	restartDelayMs int,
-	deviceID string,
-	waitGroup *sync.WaitGroup) {
-	waitGroup.Add(1)
-	go func() {
-		defer waitGroup.Done()
-
-		for {
-			exitNow := launchProcessAux(pathToExecutable, arguments, inputMessages, mqttMessages, deviceID)
-			if exitNow {
-				break
-			}
-			if !autoRestart {
-				fmt.Printf("Process completed...\n")
-				break
-			} else {
-				fmt.Printf("Process exited. Auto restarting\n")
-				time.Sleep(time.Duration(restartDelayMs) * time.Millisecond)
-			}
-		}
-	}()
-}
-
-func launchProcessAux(pathToExecutable string, arguments string, inputMessages chan string, mqttMessages chan MQTTMessage, deviceID string) bool {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	cmd := exec.CommandContext(ctx, pathToExecutable, arguments)
-
-	_, err := cmd.StdinPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	out, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ch := make(chan error)
-	go func() {
-		mqttMessages <- MQTTMessage{PUBLISH, "Process started at: " + time.Now().String(), fmt.Sprintf("devices/%v/start", deviceID), 2}
-		runResult := cmd.Run()
-		mqttMessages <- MQTTMessage{PUBLISH, "Process stopped at: " + time.Now().String(), fmt.Sprintf("devices/%v/stop", deviceID), 2}
-		ch <- runResult
-	}()
-
-	buf := bufio.NewReader(out) // Notice that this is not in a loop
-	var currentLine []byte
-
-	for {
-		select {
-		case err = <-ch:
-			return false
-		case command := <-inputMessages:
-			if command == "shutdown" {
-				print("shutdown process now\n")
-				return true
-			}
-		default:
-		}
-
-		bytes, err := buf.ReadBytes('\n')
-		if err == io.EOF {
-			currentLine = append(currentLine, bytes...)
-		} else if err != nil {
-			break // some other nasty error
-		} else {
-			currentLine = append(currentLine, bytes...)
-			print(string(currentLine))
-			mqttMessages <- MQTTMessage{PUBLISH, string(currentLine), fmt.Sprintf("devices/%v/stdout", deviceID), 2}
-			currentLine = []byte{}
-		}
-	}
-	return false
 }
---
+++ agent/Process.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os/exec"
+	"sync"
+	"time"
+)
+
+func LaunchProcess(
+	pathToExecutable string,
+	arguments string,
+	inputMessages chan string,
+	mqttMessages chan MQTTMessage,
+	autoRestart bool,
+	restartDelayMs int,
+	deviceID string,
+	waitGroup *sync.WaitGroup) {
+	waitGroup.Add(1)
+	go func() {
+		defer waitGroup.Done()
+
+		for {
+			exitNow := launchProcessAux(pathToExecutable, arguments, inputMessages, mqttMessages, deviceID)
+			if exitNow {
+				break
+			}
+			if !autoRestart {
+				fmt.Printf("Process completed...\n")
+				break
+			} else {
+				fmt.Printf("Process exited. Auto restarting\n")
+				time.Sleep(time.Duration(restartDelayMs) * time.Millisecond)
+			}
+		}
+	}()
+}
+
+func launchProcessAux(pathToExecutable string, arguments string, inputMessages chan string, mqttMessages chan MQTTMessage, deviceID string) bool {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, pathToExecutable, arguments)
+
+	_, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ch := make(chan error)
+	go func() {
+		mqttMessages <- MQTTMessage{PUBLISH, "Process started at: " + time.Now().String(), fmt.Sprintf("devices/%v/start", deviceID), 2}
+		runResult := cmd.Run()
+		mqttMessages <- MQTTMessage{PUBLISH, "Process stopped at: " + time.Now().String(), fmt.Sprintf("devices/%v/stop", deviceID), 2}
+		ch <- runResult
+	}()
+
+	buf := bufio.NewReader(out) // Notice that this is not in a loop
+	var currentLine []byte
+
+	for {
+		select {
+		case err = <-ch:
+			return false
+		case command := <-inputMessages:
+			if command == "shutdown" {
+				print("shutdown process now\n")
+				return true
+			}
+		default:
+		}
+
+		bytes, err := buf.ReadBytes('\n')
+		if err == io.EOF {
+			currentLine = append(currentLine, bytes...)
+		} else if err != nil {
+			break // some other nasty error
+		} else {
+			currentLine = append(currentLine, bytes...)
+			print(string(currentLine))
+			mqttMessages <- MQTTMessage{PUBLISH, string(currentLine), fmt.Sprintf("devices/%v/stdout", deviceID), 2}
+			currentLine = []byte{}
+		}
+	}
+	return false
+}
